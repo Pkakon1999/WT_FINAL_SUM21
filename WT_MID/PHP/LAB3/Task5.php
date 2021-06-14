424ef5dd570aa9bd941819f09cf45b5bceda9c68
Kakon Paul Avi <pkakon1816@gmail.com>
2021-06-14 11:01:54 +0600

    Lab 3

---
+++ WT_MID/PHP/LAB3/Task5.php
@@ -0,0 +1,18 @@
+<?php
+	$backColor = "green";
+	$fontColor = "red";
+	$imageWidth = "80px";
+	$imageHeight = "80px";
+?>
+
+<table>
+	<tr>
+		<td>
+			<img src = "aiub_logo.png" width = "<?php echo $imageWidth;?>" height = "<?php echo $imageHeight;?>" />
+		</td>
+		
+		<td>
+			<h1> American International <br> University - <span style = "background-color: <?php echo $backColor ?>; color: <?php echo $fontColor ?>;"> Bangladesh </span> </h1>
+		</td>
+	</tr>
+</table>
